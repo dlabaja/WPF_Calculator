b75e6e0a28d0ec3728a2d2f61c1e1e2a29a6a3b5
dlabaja <dlabaja.jan@seznam.cz>
2023-05-13 23:32:02 +0200

    styling pain

---
+++ WPFCalculator/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WPFCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WPFCalculator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace WPFCalculator
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            
+        }
+        
+        public void WriteConstant(object sender, RoutedEventArgs e)
+        {
+                
+        }
+
+        public void WriteToInput(object sender, RoutedEventArgs e)
+        {
+                
+        }
+        
+        public void WriteDigit(object sender, RoutedEventArgs e)
+        {
+                
+        }
+        
+        public void WriteSymbol(object sender, RoutedEventArgs e)
+        {
+                
+        }
+        
+        public void SendToServer(object sender, RoutedEventArgs e)
+        {
+                
+        }
+
+        public void DeleteChar(object sender, RoutedEventArgs e)
+        {
+                
+        }
+        
+        public void ClearInput(object sender, RoutedEventArgs e)
+        {
+                
+        }
+    }
+}
