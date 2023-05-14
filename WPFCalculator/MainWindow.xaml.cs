e442ab7266be25ac241ea22b1570f2f8248a88d1
dlabaja <dlabaja.jan@seznam.cz>
2023-05-14 13:56:07 +0200

    logika

--- WPFCalculator/MainWindow.xaml.cs
+++ WPFCalculator/MainWindow.xaml.cs
@@ -1,47 +1,69 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using PostfixCalculator = WPFCalculator.PostfixCalculator;
 
 namespace WPFCalculator
 {
     public partial class MainWindow
     {
-        public MainWindow()
+        private string _ans = "0";
+        private const string operators = "+-*/%";
+        private string[] constants = {"π", "e", "Ans"};
+
+        private void WriteToInput(string val)
         {
-            
-        }
-        
-        public void WriteConstant(object sender, RoutedEventArgs e)
-        {
-                
+            Input.Text += val;
         }
 
-        public void WriteToInput(object sender, RoutedEventArgs e)
+        private void WriteFuncConst(object sender, RoutedEventArgs e)
         {
-                
-        }
-        
-        public void WriteDigit(object sender, RoutedEventArgs e)
-        {
-                
-        }
-        
-        public void WriteSymbol(object sender, RoutedEventArgs e)
-        {
-                
-        }
-        
-        public void SendToServer(object sender, RoutedEventArgs e)
-        {
-                
+            if (operators.Contains(GetLastChar()) || GetLastChar() == default || GetLastChar() == '(')
+                WriteToInput(((Button) sender).Tag.ToString());
         }
 
-        public void DeleteChar(object sender, RoutedEventArgs e)
+        private void WriteAnything(object sender, RoutedEventArgs e)
         {
-                
+            WriteToInput(((Button) sender).Tag.ToString());
         }
-        
-        public void ClearInput(object sender, RoutedEventArgs e)
+
+        private void WriteDecimalPoint(object sender, RoutedEventArgs e)
         {
-                
+            if (char.IsDigit(GetLastChar()))
+                WriteToInput(".");
+        }
+
+        private void WriteOperator(object sender, RoutedEventArgs e)
+        {
+            if (!operators.Contains(GetLastChar()))
+                WriteToInput(((Button) sender).Tag.ToString());
+        }
+
+        private void SendToServer(object sender, RoutedEventArgs e)
+        {
+            Input.Text = Input.Text.Replace("Ans", _ans);
+            Output.Text = new PostfixCalculator().CalculatePostfix(
+                new PostfixCalculator().InfixToPostfix(new StringBuilder(Input.Text))).ToString();
+            _ans = Output.Text;
+        }
+
+        private void DeleteChar(object sender, RoutedEventArgs e)
+        {
+            if (Input.Text.Length == 0) return;
+            Input.Text = Input.Text.Substring(0, Input.Text.Length - 1);
+        }
+
+        private void ClearInput(object sender, RoutedEventArgs e)
+        {
+            Input.Text = "";
+        }
+
+        private char GetLastChar()
+        {
+            return Input.Text.Length == 0 ? default : Input.Text[Input.Text.Length - 1];
         }
     }
 }
